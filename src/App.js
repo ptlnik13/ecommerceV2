3f835a96220060c9ae82313a192799ba514a7096
ptlnik13 <nikeshvpatel@gmail.com>
2023-02-12 15:31:28 -0500

    88. Styling for nav + logo

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
-import DirectoryComponent from "./components/directory/Directory.component";
+import {Routes, Route} from "react-router-dom";
+import HomeRoutes from "./routes/home/Home.routes";
+import NavigationRoutes from "./routes/navigation/Navigation.routes";
+
+
+
+
+const ShopRoutes = ()=>{
+return(
+    <div>
+        <h2>I am shop page</h2>
+    </div>
+)
+}
 
 const App = () => {
-    const categories = [
-        {
-            "id": 1,
-            "title": "hats",
-            "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-        },
-        {
-            "id": 2,
-            "title": "jackets",
-            "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-        },
-        {
-            "id": 3,
-            "title": "sneakers",
-            "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-        },
-        {
-            "id": 4,
-            "title": "womens",
-            "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-        },
-        {
-            "id": 5,
-            "title": "mens",
-            "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-        }
-    ];
-
     return (
-        <DirectoryComponent categories={categories}/>
-    );
+        <Routes>
+            <Route path='/' element={<NavigationRoutes/>}>
+                <Route index element={<HomeRoutes/>} />
+                <Route path='shop' element={<ShopRoutes/>} />
+            </Route>
+        </Routes>
+    )
 }
 
 export default App;
