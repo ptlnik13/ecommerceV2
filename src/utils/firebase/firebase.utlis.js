eb3d68d777280f7d068686d6e380720d1cb177cc
ptlnik13 <nikeshvpatel@gmail.com>
2023-02-18 15:16:32 -0500

    96. Finish Creating user Document

--- src/utils/firebase/firebase.utlis.js
+++ src/utils/firebase/firebase.utlis.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
-
+import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,4 +21,21 @@ provider.setCustomParameters({prompt: 'select_account'})
 
 
 export const auth = getAuth();
-export const signInWithGooglePopup = ()=> signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid)
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, {displayName, email, createdAt});
+        } catch (e) {
+            console.log('error while creating user', e.message);
+        }
+    }
+    return userDocRef;
+}
