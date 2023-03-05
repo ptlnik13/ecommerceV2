dc12782b69ce6ba70a79d1c0ffcc8d0f1f30fba7
ptlnik13 <nikeshvpatel@gmail.com>
2023-03-05 12:26:46 -0500

    154. Setting up Store and Redux template

--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {CategoriesProvider} from "./context/categoriesContext";
 import {CartProvider} from "./context/cart.context";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <UserProvider>
-                <CategoriesProvider>
-                    <CartProvider>
-                        <App/>
-                    </CartProvider>
-                </CategoriesProvider>
-            </UserProvider>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <UserProvider>
+                    <CategoriesProvider>
+                        <CartProvider>
+                            <App/>
+                        </CartProvider>
+                    </CategoriesProvider>
+                </UserProvider>
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
 
---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,23 @@
+
+const INITIAL_STATE = {
+    currentUser: null,
+}
+
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state = INITIAL_STATE, {type, payload})=>{
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return{
+                ...state,
+                currentUser: payload
+            }
+        default:
+           return state
+    }
+}
+
+export default userReducer;
