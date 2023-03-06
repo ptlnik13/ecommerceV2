83290f750008372253d7c5d897ac2a98b0e360cf
ptlnik13 <nikeshvpatel@gmail.com>
2023-03-06 11:45:38 -0500

    156. adding useSelector for user redux

--- src/routes/navigation/Navigation.routes.jsx
+++ src/routes/navigation/Navigation.routes.jsx
@@ -1,6 +1,5 @@
 import {Fragment, useContext} from "react";
-import {Link, Outlet} from "react-router-dom";
-import {UserContext} from "../../context/user.context";
+import {Outlet} from "react-router-dom";
 import {CartContext} from "../../context/cart.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 import {ReactComponent as CrwnLogo} from "../../assets/images/crown.svg";
@@ -8,9 +7,11 @@ import CartIconComponent from "../../components/cart-icon/cart-icon.component";
 import CartDropdownComponent from "../../components/cart-dropdown/cart-dropdown.component";
 
 import {NavigationContainer, NavLink, NavLinks, LogoContainer} from "./navigation.styles";
+import {useSelector} from "react-redux";
+import {selectCurrentUser} from "../../store/user/user.selectors";
 
 const NavigationRoutes = () => {
-    const {currentUser} = useContext(UserContext);
+    const currentUser = useSelector(selectCurrentUser)
     const {isCartOpen} = useContext(CartContext);
     const signOutHandler = async () => {
         await signOutUser();
