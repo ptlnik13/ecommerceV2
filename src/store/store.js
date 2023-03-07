2391e21809361223b71306c3d6c270dc9125f0d3
ptlnik13 <nikeshvpatel@gmail.com>
2023-03-07 12:55:32 -0500

    170. Adding redux devtools in dev env

--- src/store/store.js
+++ src/store/store.js
@@ -14,6 +14,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middlewares = [process.env.NODE_ENV !== 'development' && logger].filter(Boolean);
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) // if dev tools exists the use this compose.
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 export const store = createStore(persistedReducer, undefined, composedEnhancers)
 export const persistor = persistStore(store);
