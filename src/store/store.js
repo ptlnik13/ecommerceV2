b73fda9354c246bdd37942dcd1a3b591a9e2513f
ptlnik13 <nikeshvpatel@gmail.com>
2023-03-07 12:32:23 -0500

    169. moving into Persistor

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,19 @@
 import {compose, createStore, applyMiddleware} from "redux";
 import {logger} from "redux-logger/src";
+import {persistStore, persistReducer} from "redux-persist";
+import Storage from 'redux-persist/lib/storage'
 
 import {rootReducer} from "./root-reducer";
 
+const persistConfig = {
+    key: 'root',
+    storage: Storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const middlewares = [logger]
 const composedEnhancers = compose(applyMiddleware(...middlewares));
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+export const persistor = persistStore(store);
